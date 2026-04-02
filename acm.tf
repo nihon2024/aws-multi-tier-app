7424e53e2531d86b156400d518e4e4224e2a83b2
Nitesh Pandey <niteshkumaruk@hotmail.com>
2026-04-02 14:23:02 +0100

    Final cleanup: standardised Terraform structure (infra-only)

--- acm.tf
+++
@@ -1,9 +0,0 @@
-# acm.tf
-resource "aws_acm_certificate" "alb_cert" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  tags = {
-    Name = "${var.project_name}-alb-cert"
-  }
-}
--- main.tf
+++
@@ -1,6 +0,0 @@
-# main.tf
-
-# Generate a random suffix for unique resource names if needed later
-resource "random_id" "suffix" {
-  byte_length = 4
-}
